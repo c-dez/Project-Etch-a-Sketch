1c8391f6fa21fb80a12dbe486dbb3563c38bab13
c-dez <hdezcruzcarlos@gmail.com>
2023-05-18 23:04:33 -0600

    grid hecho

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log('hello');
+//create a 10x10 of square divs in js
+//put them in another div container
+//they need to be a grid---flex wrap
+
+//funcion tama;o grid
+
+const width =500;
+const userSize= 10;
+let gridSize = width/userSize;
+
+
+
+
+const container = document.querySelector('#container');
+container.style.display =('flex');
+container.style.flexDirection = ('row');
+container.style.flexWrap = ('wrap');
+container.style.height = ('500px');
+container.style.width = ('500px');
+container.style.border =('5px solid pink');
+
+createGrid(userSize);
+
+// grid.style.backgroundColor =('trasparent');
+
+function createGrid(userSize){
+    for(let i =1; i<=(userSize*userSize);i++){
+        const grid = document.createElement('div');
+        container.appendChild(grid);
+        grid.style.border = ('1px solid grey');
+        // grid.style.flex =('1')
+        grid.style.boxSizing = ('border-box');
+        grid.style.height = (gridSize+'px');
+        grid.style.width = (gridSize+'px');
+    
+    
+    }
+    }
+    
+
+
