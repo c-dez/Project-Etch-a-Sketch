444b019bb2d88ef73e744e34c2aeab1409f0496f
c-dez <hdezcruzcarlos@gmail.com>
2023-05-21 15:38:44 -0600

    added gridGenerator() working

--- app.js
+++ app.js
@@ -2,35 +2,66 @@
 const container = document.querySelector('#container');
 container.style.display = ('flex');
 container.style.gap = ('10px');
-//menuBar
-const menuBar = document.createElement('div');
-container.appendChild(menuBar);
-menuBar.style.display = ('flex');
-menuBar.style.flexDirection = ('column');
-menuBar.style.border = ('1px solid red')
-menuBar.style.height = ('500px');
-menuBar.style.width = ('200px');
-//menuBar buttons
+//menuContainer
+const menuContainer = document.createElement('div');
+menuContainer.className = ('menuContainer');
+container.appendChild(menuContainer);
+menuContainer.style.display = ('flex');
+menuContainer.style.flexDirection = ('column');
+// menuContainer.style.flex = ('auto');
+menuContainer.style.border = ('1px solid red')
+menuContainer.style.height = ('500px');
+menuContainer.style.width = ('200px');
+//menuContainer buttons
 //btn8
 const btn8 = document.createElement('button');
-menuBar.appendChild(btn8);
+btn8.className = ('btn8');
+menuContainer.appendChild(btn8);
 btn8.textContent = ('8x8');
 //16x16
 const btn16= document.createElement('button');
-menuBar.appendChild(btn16);
+btn16.className = ('btn16');
+menuContainer.appendChild(btn16);
 btn16.textContent = ('16x16');
 //32x32
 const btn32 = document.createElement('button');
-menuBar.appendChild(btn32);
+btn32.className = ('btn32');
+menuContainer.appendChild(btn32);
 btn32.textContent = ('32x32');
 
 
-//grid
-const grid = document.createElement('div');
-container.appendChild(grid);
-grid.style.border = ('1px solid red')
-grid.style.height = ('500px');
-grid.style.width = ('500px');
+//gridContainer
+const gridContainer = document.createElement('div');
+gridContainer.className = ('gridContainer');
+container.appendChild(gridContainer);
+gridContainer.style.border = ('1px solid red')
+gridContainer.style.height = ('500px');
+gridContainer.style.width = ('500px');
+gridContainer.style.display = ('flex');
+gridContainer.style.flexDirection = ('column');
+gridContainer.style.flexShrink = ('0');
+gridContainer.style.flexWrap = ('wrap');
+
+
+//gridGenerator empezar con 8x8
+//tama;o de cada div----gridContainer.heigth/8
+
+function gridGenerator(){
+let gridSize = 500/8;
+
+    for(let i = 0; i<=8**2; i++){
+        const grid = document.createElement('div');
+        gridContainer.appendChild(grid);
+        grid.className = ('grid');
+        grid.style.border = ('1px solid gray')
+        grid.style.boxSizing = ('border-box');
+        grid.style.height =('62.5px');
+        grid.style.width =('62.5px');
+
+
+    }
+}
+gridGenerator();
 
 
 
